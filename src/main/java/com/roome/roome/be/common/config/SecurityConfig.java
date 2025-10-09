2442002dd00651ab5438f39f9db80543d16e2633
shinyj0 <yejins0605@gmail.com>
2025-10-09 19:39:38 +0900

    chore: CORS 허용 도메인(roome.p-e.kr) 추가

--- src/main/java/com/roome/roome/be/common/config/SecurityConfig.java
+++ src/main/java/com/roome/roome/be/common/config/SecurityConfig.java
@@ -78,7 +78,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:8080"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:8080","https://roome.p-e.kr"));
         config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
