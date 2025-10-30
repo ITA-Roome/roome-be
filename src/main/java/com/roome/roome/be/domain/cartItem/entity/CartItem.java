2aabeabdc325dd22c0a6d18974129061536f3d96
shinyj0 <yejins0605@gmail.com>
2025-10-30 10:15:53 +0900

    feat: CartItem entity 구현

---
+++ src/main/java/com/roome/roome/be/domain/cartItem/entity/CartItem.java
@@ -0,0 +1,42 @@
+package com.roome.roome.be.domain.cartItem.entity;
+
+import com.roome.roome.be.common.base.BaseEntity;
+import com.roome.roome.be.domain.product.entity.Product;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class CartItem extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "product_id", nullable = false)
+	private Product product;
+
+	@Column(nullable = false)
+	private int quantity;
+
+	public void updateQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+}
