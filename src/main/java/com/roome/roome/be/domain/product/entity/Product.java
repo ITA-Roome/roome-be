7da0ff1c033119cce6b870f4095092f0d62b427b
shinyj0 <yejins0605@gmail.com>
2025-10-30 12:38:26 +0900

    refactor: Enum 타입 필드에 @Enumerated(EnumType.STRING) 추가

--- src/main/java/com/roome/roome/be/domain/product/entity/Product.java
+++ src/main/java/com/roome/roome/be/domain/product/entity/Product.java
@@ -6,6 +6,8 @@ import com.roome.roome.be.domain.product.enums.Color;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -32,9 +34,11 @@ public class Product extends BaseEntity {
 	@Column(nullable = false)
 	private int price;
 
+	@Enumerated(EnumType.STRING)
 	@Column(nullable = false)
 	private Category category;
 
+	@Enumerated(EnumType.STRING)
 	@Column(nullable = false)
 	private Color color;
 
