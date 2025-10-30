2f80c8b3712c2291b13044c8b2dad91c82b86021
shinyj0 <yejins0605@gmail.com>
2025-10-31 01:43:39 +0900

    feat: ShopRepository 구현

---
+++ src/main/java/com/roome/roome/be/domain/shop/repository/ShopRepository.java
@@ -0,0 +1,9 @@
+package com.roome.roome.be.domain.shop.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.roome.roome.be.domain.shop.entity.Shop;
+
+public interface ShopRepository extends JpaRepository<Shop, Long> {
+
+}
