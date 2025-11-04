88b17c5deddab1a5bd17a6c6a28c5c834ee6dd5a
shinyj0 <yejins0605@gmail.com>
2025-11-04 15:40:52 +0900

    feat: Product entity에 추가적인 컬럼 추가

--- src/main/java/com/roome/roome/be/domain/product/entity/Product.java
+++ src/main/java/com/roome/roome/be/domain/product/entity/Product.java
@@ -3,14 +3,18 @@ package com.roome.roome.be.domain.product.entity;
 import com.roome.roome.be.common.base.BaseEntity;
 import com.roome.roome.be.domain.product.enums.Category;
 import com.roome.roome.be.domain.product.enums.Color;
+import com.roome.roome.be.domain.shop.entity.Shop;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -41,5 +45,15 @@ public class Product extends BaseEntity {
 	@Enumerated(EnumType.STRING)
 	@Column(nullable = false)
 	private Color color;
+
+	@Column(columnDefinition = "TEXT")
+	private String description;
+
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "shop_id", nullable = false)
+	private Shop shop;
+
+	@Column(length = 512)
+	private String thumbnailKey;
 
 }
