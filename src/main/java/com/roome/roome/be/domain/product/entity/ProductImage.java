6fa4b787cef8fa6a9b5087375e9d3930c973973f
shinyj0 <yejins0605@gmail.com>
2025-11-04 14:04:47 +0900

    feat: ProductImage entity 구현

---
+++ src/main/java/com/roome/roome/be/domain/product/entity/ProductImage.java
@@ -0,0 +1,39 @@
+package com.roome.roome.be.domain.product.entity;
+
+import com.roome.roome.be.common.base.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class ProductImage extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "product_id", nullable = false)
+	private Product product;
+
+	@Column(nullable = false, length = 512, unique = true)
+	private String objectKey;
+
+	@Column(nullable = false)
+	private int orderIndex;
+}
