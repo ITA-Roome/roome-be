cae9fffe3e5b0ca9dc453813ce50764bc7b59615
shinyj0 <yejins0605@gmail.com>
2025-11-07 13:01:41 +0900

    refactor: tag, product 엔티티 수정

--- src/main/java/com/roome/roome/be/domain/product/entity/Product.java
+++ src/main/java/com/roome/roome/be/domain/product/entity/Product.java
@@ -1,10 +1,14 @@
 package com.roome.roome.be.domain.product.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.roome.roome.be.common.base.BaseEntity;
 import com.roome.roome.be.domain.product.enums.Category;
 import com.roome.roome.be.domain.product.enums.Color;
 import com.roome.roome.be.domain.shop.entity.Shop;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -15,6 +19,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -32,7 +37,7 @@ public class Product extends BaseEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(nullable = false, length = 50)
+	@Column(nullable = false, length = 255)
 	private String name;
 
 	@Column(nullable = false)
@@ -41,10 +46,6 @@ public class Product extends BaseEntity {
 	@Enumerated(EnumType.STRING)
 	@Column(nullable = false)
 	private Category category;
-
-	@Enumerated(EnumType.STRING)
-	@Column(nullable = false)
-	private Color color;
 
 	@Column(columnDefinition = "TEXT")
 	private String description;
@@ -56,10 +57,17 @@ public class Product extends BaseEntity {
 	@Column(length = 512)
 	private String thumbnailKey;
 
+	@Column(name = "product_url", length = 1024, nullable = false)
+	private String productUrl;
+
+	@OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+
+	@Builder.Default
+	private List<ProductTag> productTags = new ArrayList<>();
+
 	public void updateName(String name) { this.name = name; }
 	public void updatePrice(Integer price) { this.price = price; }
 	public void updateCategory(Category category) { this.category = category; }
-	public void updateColor(Color color) { this.color = color; }
 	public void updateDescription(String description) { this.description = description; }
 	public void updateThumbnail(String thumbnailKey) { this.thumbnailKey = thumbnailKey; }
 
--- src/main/java/com/roome/roome/be/domain/product/entity/Tag.java
+++ src/main/java/com/roome/roome/be/domain/product/entity/Tag.java
@@ -1,10 +1,16 @@
 package com.roome.roome.be.domain.product.entity;
 
+import com.roome.roome.be.domain.product.enums.TagType;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,12 +22,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
+@Table(
+	name = "tag",
+	uniqueConstraints = @UniqueConstraint(columnNames = {"name", "type"})
+)
 public class Tag {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false, length = 30)
+	private TagType type;
+
 	@Column(nullable = false, length = 50)
 	private String name;
-
 }
