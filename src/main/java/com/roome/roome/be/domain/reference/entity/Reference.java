408f2820ad74edea355f007345420167693bde88
pooreumjung <pooreumjung02@naver.com>
2025-11-18 12:41:15 +0900

    feat: Reference, ReferenceImage, UserScrapReference Entity 추가

---
+++ src/main/java/com/roome/roome/be/domain/reference/entity/Reference.java
@@ -0,0 +1,30 @@
+package com.roome.roome.be.domain.reference.entity;
+
+import com.roome.roome.be.common.base.BaseEntity;
+import com.roome.roome.be.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Reference extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "like_count",nullable = false)
+    private Integer likeCount;
+
+    @OneToMany(mappedBy = "reference", fetch = FetchType.LAZY)
+    private List<ReferenceImage> referenceImageList = new ArrayList<>();
+}
---
+++ src/main/java/com/roome/roome/be/domain/reference/entity/ReferenceImage.java
@@ -0,0 +1,23 @@
+package com.roome.roome.be.domain.reference.entity;
+
+import com.roome.roome.be.common.base.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class ReferenceImage extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "reference_id", nullable = false)
+    private Reference reference;
+
+    @Column(nullable = false, length = 512, unique = true)
+    private String imageUrl;
+}
