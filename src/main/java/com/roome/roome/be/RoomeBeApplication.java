e6c7814f57761392840eee98bb9014293a9f4ea6
shinyj0 <yejins0605@gmail.com>
2025-11-21 00:46:39 +0900

    chore: redis 관련 환경 설정

--- src/main/java/com/roome/roome/be/RoomeBeApplication.java
+++ src/main/java/com/roome/roome/be/RoomeBeApplication.java
@@ -2,10 +2,12 @@ package com.roome.roome.be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableCaching
 public class RoomeBeApplication {
 
     public static void main(String[] args) {
