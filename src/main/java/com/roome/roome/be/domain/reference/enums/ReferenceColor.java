734ca605bcd41b4f392f6a9125a50792f1bef598
Hheojiwon <0618amy@naver.com>
2026-01-09 05:19:50 +0900

    feat: 레퍼런스 컬러 enum 추가

--- src/main/java/com/roome/roome/be/domain/reference/enums/ReferenceColor.java
+++ src/main/java/com/roome/roome/be/domain/reference/enums/ReferenceColor.java
@@ -1,5 +1,20 @@
 package com.roome.roome.be.domain.reference.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum ReferenceColor {
 
+    BRIGHT_TONE("밝은 톤"),
+    DARK_TONE("어두운 톤"),
+    WARM_TONE("따뜻한 톤"),
+    GRAY_TONE("그레이 톤"),
+    CALM_TONE("차분한 톤"),
+    NEUTRAL("뉴트럴"),
+    COLORFUL("컬러풀/비비드"),
+    NO_PREFERENCE("상관없음");
+
+    private final String description;
 }
